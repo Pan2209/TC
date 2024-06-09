808480cb7d592c504690f338d29b50e2d6d8cddf
Angel Zuniga <mazuniga@sudamericano.edu.ec>
2024-06-08 20:14:23 -0500

    asda

---
+++ 25-ARRAYS/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+let employee = ["Marco","Sofia","Roberto","carlos","Fabian","Martha","Ruben","Victoria",]
+
+for (let i=0;1<8;i++){
+        alert("Los empleados de esta sucursal son: " + employee[i])
+}

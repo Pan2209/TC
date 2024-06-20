46d363428e6e0d00adb346eb3da94c0b7fc4e9ef
Angel Zuniga <mazuniga@sudamericano.edu.ec>
2024-06-20 09:31:00 -0500

    comments deleted

--- 33-EJERCICIOS-CONCATENAR-ELEMENTOS/EJERCICIO3/mainn.js
+++ 33-EJERCICIOS-CONCATENAR-ELEMENTOS/EJERCICIO3/mainn.js
@@ -14,16 +14,4 @@ function concatenarNumerosContinuos(nums) {
 }
 
 concatenarNumerosContinuos(nums);
-    
-/*let nums;
-nums = [0, 2, 4, 8];
-let result;
-function concatenarNumeros(nums) {
-    for (let i = 0; i < nums.length; i++)
-        for (let x = 0; x < nums.length; x++)
-     
-      
-
-     = (nums[i] + '' + nums[x])
-            alert(result)
-}*/
+  

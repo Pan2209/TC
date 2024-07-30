6b0a098dec9eaa8bacb7fb07f74fa9ae5bdeba5f
Angel Zuniga <mazuniga@sudamericano.edu.ec>
2024-07-30 07:25:23 -0500

    ´actualizacion´

--- 35-TIPOS-FOR/main.js
+++ 35-TIPOS-FOR/main.js
@@ -4,8 +4,8 @@ for(let i=0; i<nums.lenght; i++){
     alert(`indice: ${i} elemento ${nums[i]}`)
 }
 
-
-//Crear una funcion que filtre los valores mayores a 10.00 y redondearlos a 2 decimales como string. Retornando un array.
+  //Crear una funcion que filtre los valores mayores a 10.00 y
+//  redondearlos a 2 decimales como string. Retornando un array.
 
 nums = [13.676767, 3.123432, 5.34543543, 10.3452345]
 
